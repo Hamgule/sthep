5746da3108dd0cc568ffc1f09d3d299b8274379f
JihuYang <zihooy139@gmail.com>
2022-03-19 00:13:26 +0900

    create sidebar: add profile

--- lib/main_page/main_page.dart
+++ lib/main_page/main_page.dart
@@ -1,22 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:sthep/model/user/user.dart';
+
+bool isGrid = true;
+User tempUser = User(
+  id: 'zihoo1234',
+  name: '양지후',
+  nickname: 'zihoo',
+  password: '',
+);
 
 PreferredSizeWidget mainPageAppBar = AppBar(
   backgroundColor: Colors.white,
   foregroundColor: Colors.black,
-  title:
-    Image.asset(
-      'assets/images/logo_horizontal.png',
-      fit: BoxFit.contain,
-      width: 150,
+  title: Image.asset(
+    'assets/images/logo_horizontal.png',
+    fit: BoxFit.contain,
+    width: 150,
+  ),
+  actions: [
+    IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+    StatefulBuilder(
+      builder: (context, setState) => IconButton(
+        onPressed: () => setState(() => isGrid = !isGrid),
+        icon: Icon(
+          isGrid ? Icons.list_alt : Icons.window,
+        ),
+      ),
     ),
-  actions: const [
-    Icon(Icons.search,),
-    Icon(Icons.list,),
-    Icon(Icons.window,),
+    StatefulBuilder(
+      builder: (context, setState) => IconButton(
+        onPressed: () => Scaffold.of(context).openEndDrawer(),
+        icon: const Icon(Icons.menu),
+      ),
+    ),
   ],
-
 );
-
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -30,6 +48,112 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: mainPageAppBar,
+      endDrawer: const SideBar(),
     );
   }
 }
+
+Widget profile(User user) => Row(
+  children: [
+    Container(
+        width: 50.0,
+        height: 50.0,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image: user.image.image,
+          ),
+        )
+    ),
+    const SizedBox(width: 15.0),
+    Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(bottom: 3),
+            child: Text(
+              user.id,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Text(
+            '${user.name} / ${user.nickname}',
+            style: TextStyle(
+              color: Colors.grey[500],
+            ),
+          ),
+        ],
+      ),
+    ),
+  ],
+);
+
+class SideBar extends StatefulWidget {
+  const SideBar({Key? key}) : super(key: key);
+  @override
+  _SideBarState createState() => _SideBarState();
+}
+
+class _SideBarState extends State<SideBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            child: Container(
+              child: Column(
+                children: [
+                  const Text(
+                    '나의 정보',
+                    style: TextStyle(fontSize: 20.0,),
+                    textAlign: TextAlign.start,
+                  ),
+                  const Padding(padding: EdgeInsets.fromLTRB(0.0, 50.0, 0.0, 0.0)),
+                  Row(
+                    children: [
+                      Expanded(child: profile(tempUser)),
+                      const Icon(Icons.settings),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            decoration: const BoxDecoration(
+              color: Colors.blue,
+            ),
+          ),
+  
+          //Step 3, set child widgets in drawer
+          ListTile(title: Text('Item 1')),
+          ListTile(title: Text('Item 2')),
+          ListTile(title: Text('Item 3')),
+        ],
+      ),
+    );
+  }
+}
+// class SideBar extends StatelessWidget {
+//   const SideBar({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Drawer(
+//       child: ListView(children: const [
+//         DrawerHeader(
+//           child: Text('My Page'),
+//           decoration: BoxDecoration(
+//             color: Colors.indigoAccent,
+//           ),
+//         ),
+//         ListTile(title: Text('Item 1')),
+//         ListTile(title: Text('Item 2')),
+//         ListTile(title: Text('Item 3')),
+//       ]),
+//     );
+//   }
+// }
--- lib/model/user/user.dart
+++ lib/model/user/user.dart
@@ -1,18 +1,26 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
 import 'package:sthep/model/user/exp.dart';
 
 class User {
   /// variables
   String id;
+  String name;
   String nickname;
   String password;
+  Image image = Image.network("https://i.imgur.com/BoN9kdC.png");
 
   Exp exp = Exp();
 
   /// constructor
   User({
     required this.id,
+    required this.name,
     required this.nickname,
     required this.password,
   });
+
+  // void setImage(String src) => image = Image.network("https://i.imgur.com/BoN9kdC.png");
 
 }
