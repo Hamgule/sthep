b8e7155572e7106d8205afacb9c1f4a119faff24
JihuYang <zihooy139@gmail.com>
2022-03-23 21:25:38 +0900

    Update pubspec.yaml: merge conflict

--- lib/main_page/main_page.dart
+++ lib/main_page/main_page.dart
@@ -54,43 +54,42 @@ class _MainPageState extends State<MainPage> {
 }
 
 Widget profile(User user) => Row(
-  children: [
-    Container(
-        width: 50.0,
-        height: 50.0,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: user.image.image,
-          ),
-        )
-    ),
-    const SizedBox(width: 15.0),
-    Expanded(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: const EdgeInsets.only(bottom: 3),
-            child: Text(
-              user.id,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
+      children: [
+        Container(
+            width: 50.0,
+            height: 50.0,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: user.image.image,
               ),
-            ),
+            )),
+        const SizedBox(width: 15.0),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(bottom: 3),
+                child: Text(
+                  user.id,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
+                '${user.name} / ${user.nickname}',
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              ),
+            ],
           ),
-          Text(
-            '${user.name} / ${user.nickname}',
-            style: TextStyle(
-              color: Colors.grey[500],
-            ),
-          ),
-        ],
-      ),
-    ),
-  ],
-);
+        ),
+      ],
+    );
 
 class SideBar extends StatefulWidget {
   const SideBar({Key? key}) : super(key: key);
@@ -105,33 +104,64 @@ class _SideBarState extends State<SideBar> {
       child: ListView(
         children: [
           DrawerHeader(
-            child: Container(
-              child: Column(
-                children: [
-                  const Text(
-                    '나의 정보',
-                    style: TextStyle(fontSize: 20.0,),
-                    textAlign: TextAlign.start,
-                  ),
-                  const Padding(padding: EdgeInsets.fromLTRB(0.0, 50.0, 0.0, 0.0)),
-                  Row(
-                    children: [
-                      Expanded(child: profile(tempUser)),
-                      const Icon(Icons.settings),
-                    ],
-                  ),
-                ],
-              ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [
+                    SizedBox(height: 30.0),
+                    Text(
+                      '나의 정보',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                      textAlign: TextAlign.start,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 50.0),
+                Row(
+                  children: [
+                    Expanded(child: profile(tempUser)),
+                    const Icon(Icons.settings),
+                  ],
+                ),
+              ],
             ),
             decoration: const BoxDecoration(
               color: Colors.blue,
             ),
           ),
-  
+
           //Step 3, set child widgets in drawer
-          ListTile(title: Text('Item 1')),
-          ListTile(title: Text('Item 2')),
-          ListTile(title: Text('Item 3')),
+          const ListTile(
+            title: Text('계정',
+                style: TextStyle(fontSize: 20.0, color: Colors.black)),
+          ),
+          const Divider(
+            height: 1.0,
+            thickness: 1.5,
+            indent: 20.0,
+            endIndent: 20.0,
+          ),
+          const ListTile(title: Text('아이디 변경')),
+          const ListTile(title: Text('비밀번호 변경')),
+          const ListTile(title: Text('이메일 변경')),
+          const ListTile(title: Text('로그아웃')),
+          const ListTile(title: Text('회원 탈퇴')),
+          const  ListTile(title: Text('내 활동')),
+          const ListTile(
+            title: Text('마이페이지',
+                style: TextStyle(fontSize: 20.0, color: Colors.black)),
+          ),
+          const Divider(
+            height: 1.0,
+            thickness: 1.5,
+            indent: 20.0,
+            endIndent: 20.0,
+          ),
+          const ListTile(title: Text('내 질문')),
+          const ListTile(title: Text('내 답변')),
         ],
       ),
     );
