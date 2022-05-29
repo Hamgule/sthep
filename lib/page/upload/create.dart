e94fdffbf171659acf584ea025e02b2e75c73e6a
JihuYang <zihooy139@gmail.com>
2022-05-29 23:32:10 +0900

    create 시 image painter 구현중

--- lib/page/upload/create.dart
+++ lib/page/upload/create.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:image_painter/image_painter.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:material_tag_editor/tag_editor.dart';
 import 'package:provider/provider.dart';
@@ -21,7 +22,7 @@ class _CreatePageState extends State<CreatePage> {
   final FocusNode _focusNode = FocusNode();
   final TextEditingController titleCont = TextEditingController();
   final TextEditingController tagCont = TextEditingController();
-
+  final _imageKey = GlobalKey<ImagePainterState>();
   late Question targetQuestion;
   static const double canvasPadding = 20.0;
 
@@ -41,6 +42,7 @@ class _CreatePageState extends State<CreatePage> {
 
     return Scaffold(
       body: SingleChildScrollView(
+        physics: const NeverScrollableScrollPhysics(),
         child: Container(
           padding: const EdgeInsets.all(30.0),
           child: Column(
@@ -125,32 +127,26 @@ class _CreatePageState extends State<CreatePage> {
                   ],
                 ),
               ),
-              Stack(
-                children: [
-                  Container(
-                    color: Palette.bgColor,
-                    width: screenSize.width * .90,
-                    height: 700.0,
-                  ),
-                  Positioned.fill(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Container(
-                        margin: const EdgeInsets.all(canvasPadding),
-                        color: Colors.white,
+              Padding(
+                padding: const EdgeInsets.all(canvasPadding),
+                child: SizedBox(
+                  width: screenSize.width,
+                  child: upload.image == null
+                      ? const SthepText('이미지를 선택하세요')
+                      :
+                  //Image.file(upload.image!, fit: BoxFit.fitWidth)
+                      ImagePainter.file(
+                        upload.image!,
+                        width: 300,
+                        height: 500,
+                        key: _imageKey,
+                        scalable: true,
+                        initialStrokeWidth: 2,
+                        //textDelegate: DutchTextDelegate(),
+                        initialColor: Colors.green,
+                        initialPaintMode: PaintMode.freeStyle,
                       ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(canvasPadding),
-                    child: SizedBox(
-                      width: screenSize.width * .40,
-                      child: upload.image == null
-                          ? const SthepText('이미지를 선택하세요')
-                          : Image.file(upload.image!, fit: BoxFit.fitWidth)
-                    ),
-                  ),
-                ],
+                ),
               ),
             ],
           ),
